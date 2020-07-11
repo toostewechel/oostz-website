de0e44a135ced7564eccf82bb69bdd7d5d125185
Tom Oostewechel <toostewechel@jouwomgeving.nl>
2020-07-11 14:09:46 +0200

    setup layout for slidecard component

---
+++ website/components/CardSlide.js
@@ -0,0 +1,30 @@
+function CardSlide() {
+  return (
+    <div>
+      <div className="grid grid-cols-12 grid-rows-6 bg-yellow-100 rounded overflow-hidden">
+        <div className="row-start-1 row-end-7 col-start-6 col-end-13 flex items-end">
+          <img src="/slide1.png"></img>
+        </div>
+        <div className="row-start-1 row-end-7 col-start-1 col-end-6 flex flex-col justify-center ml-8 mr-8">
+          <h1 className="text-3xl font-heading text-heading font-extrabold mb-2">
+            Sterk in ontwerp.
+          </h1>
+          <p className="text-base font-body text-body mb-6">
+            Wij werken met passie aan ontwerpen voor de verbouw, nieuwbouw en
+            aanbouw. Altijd op maat en persoonlijk.
+          </p>
+          <button className="self-start px-3 py-2 bg-yellow-500 text-heading text-sm font-body font-medium rounded hover:bg-yellow-300">
+            Lees meer
+          </button>
+        </div>
+      </div>
+      <div className="flex justify-center mt-6">
+        <div className="h-3 w-3 border-2 border-yellow-300 bg-yellow-300 mr-3 rounded-sm"></div>
+        <div className="h-3 w-3 border-2 border-gray-200 mr-3 rounded-sm"></div>
+        <div className="h-3 w-3 border-2 border-gray-200 rounded-sm"></div>
+      </div>
+    </div>
+  );
+}
+
+export default CardSlide;
--- website/pages/index.js
+++ website/pages/index.js
@@ -1,16 +1,24 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Layout from "../components/Layout";
+import CardSlide from "../components/CardSlide";
 
 export default function Home() {
   return (
     <body className="bg-background h-screen">
       <div className="container mx-auto">
         <Head>
-          <title>Oostz ontwerp</title>
+          <meta name="description" content="Oostz Ontwerp" />
+          <title>Oostz Ontwerp</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <main>
           <Header></Header>
+          <Layout>
+            <section className="mt-20">
+              <CardSlide></CardSlide>
+            </section>
+          </Layout>
         </main>
       </div>
     </body>
