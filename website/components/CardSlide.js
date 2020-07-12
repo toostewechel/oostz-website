fffff5416c330511164de6048ea21a1ce97a8f68
Tom Oostewechel <toostewechel@jouwomgeving.nl>
2020-07-12 12:47:24 +0200

    added props to CardSlide component

--- website/components/CardSlide.js
+++ website/components/CardSlide.js
@@ -1,17 +1,16 @@
-function CardSlide() {
+function CardSlide(props) {
   return (
     <div>
       <div className="flex items-center md:grid md:grid-cols-12 md:grid-rows-6 bg-yellow-100 rounded overflow-hidden">
         <div className="hidden md:row-start-1 md:row-end-7 md:col-start-6 md:col-end-13 md:flex md:items-end">
-          <img src="/slide1.png"></img>
+          <img src={props.src} alt={props.alt}></img>
         </div>
         <div className="p-6 md:row-start-1 md:row-end-7 md:col-start-1 md:col-end-6 flex flex-col justify-center md:ml-8 md:mr-8">
           <h1 className="text-3xl font-heading text-heading font-extrabold mb-2 antialiased">
-            Sterk in ontwerp.
+            {props.title}
           </h1>
           <p className="text-base font-body text-body mb-6 antialiased">
-            Wij werken met passie aan ontwerpen voor de verbouw, nieuwbouw en
-            aanbouw. Altijd op maat en persoonlijk.
+            {props.description}
           </p>
           <button className="self-start px-3 py-2 bg-yellow-500 text-heading text-sm font-body font-medium rounded hover:bg-yellow-300">
             Lees meer
