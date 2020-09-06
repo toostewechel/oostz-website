44cb9f0671004e6af31ae96958f9115c79159f73
Tom Oostewechel <tom_oostewechel@hotmail.com>
2020-09-06 11:49:06 +0200

    styling thumbnailimage

--- website/components/ThumbnailImage.js
+++ website/components/ThumbnailImage.js
@@ -18,11 +18,14 @@ function ThumbnailImage(props) {
         <div className="relative ">
           <img style={style} src={props.src} alt={props.alt}></img>
           {toggleHover && (
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
-              {/* <button
-                className="flex flex-row items-center px-3 py-2 bg-yellow-500 text-heading text-sm font-body font-medium rounded hover:bg-yellow-300"
+            <div className="absolute bottom-0 right-0">
+              <div
+                className="flex flex-row items-center px-3 py-2 bg-yellow-500 text-heading text-sm font-body font-medium rounded-br hover:bg-yellow-300"
                 onClick={props.href}
               >
+                <p className="text-heading text-sm font-body font-medium mr-2">
+                  Notaris woning
+                </p>
                 <svg
                   className=" fill-current text-background"
                   width="16"
@@ -32,7 +35,7 @@ function ThumbnailImage(props) {
                 >
                   <path d="M16 8l-3-4h-1l-1 1v2H1 0a1 1 0 000 2h11v2l1 1a1 1 0 001 0l3-4z" />
                 </svg>
-              </button> */}
+              </div>
             </div>
           )}
         </div>
