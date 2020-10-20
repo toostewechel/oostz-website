5aaeb64eed72633ce4f6f899e1872cd2bdff0894
Tom Oostewechel <tom_oostewechel@hotmail.com>
2020-10-20 19:12:32 +0200

    add next optimize images

--- website/next.config.js
+++ website/next.config.js
@@ -1,18 +1,20 @@
 require("dotenv").config();
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 
-module.exports = {
-  env: {
-    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-    AWS_REGION: process.env.AWS_REGION,
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      handleImages: ["jpeg", "png"],
+    },
+  ],
+  {
+    env: {
+      AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+      AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+      AWS_REGION: process.env.AWS_REGION,
+    },
+    trailingSlash: true,
   },
-  trailingSlash: true,
-  // exportTrailingSlash: true,
-  // exportPathMap: function () {
-  //   return {
-  //     "/": { page: "/" },
-  //     "/contact": { page: "/contact" },
-  //     "/werkwijze": { page: "/werkwijze" },
-  //   };
-  // },
-};
+]);
